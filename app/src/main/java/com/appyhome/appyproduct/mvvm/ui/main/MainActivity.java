5667929a0a14f098b25d353450354f740a03d3ac
Tran Nam <nam.tran.flash@gmail.com>
2018-01-02 12:56:04 +0700

    Layouted the home screen.
    Header and footer of home screen.

--- app/src/main/java/com/appyhome/appyproduct/mvvm/ui/main/MainActivity.java
+++ app/src/main/java/com/appyhome/appyproduct/mvvm/ui/main/MainActivity.java
@@ -22,20 +22,18 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
-import android.graphics.drawable.Animatable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -77,6 +75,7 @@ public class MainActivity extends BaseActivity<ActivityMainBinding, MainViewMode
     private Toolbar mToolbar;
     private NavigationView mNavigationView;
     private SwipePlaceHolderView mCardsContainerView;
+    private TabLayout mTabLayout;
 
     ActivityMainBinding mActivityMainBinding;
 
@@ -98,34 +97,6 @@ public class MainActivity extends BaseActivity<ActivityMainBinding, MainViewMode
         super.onResume();
         if (mDrawer != null)
             mDrawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Drawable drawable = item.getIcon();
-        if (drawable instanceof Animatable) {
-            ((Animatable) drawable).start();
-        }
-        /*switch (item.getItemId()) {
-            case R.id.action_cut:
-                return true;
-            case R.id.action_copy:
-                return true;
-            case R.id.action_share:
-                return true;
-            case R.id.action_delete:
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }*/
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -154,7 +125,7 @@ public class MainActivity extends BaseActivity<ActivityMainBinding, MainViewMode
     }
 
     private void setUp() {
-
+        mTabLayout = mActivityMainBinding.tabs;
         mDrawer = mActivityMainBinding.drawerView;
         mToolbar = mActivityMainBinding.toolbar;
         mNavigationView = mActivityMainBinding.navigationView;
@@ -162,6 +133,7 @@ public class MainActivity extends BaseActivity<ActivityMainBinding, MainViewMode
         mToolbar.setNavigationIcon(null);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayUseLogoEnabled(false);
+        getSupportActionBar().setTitle("");
         ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(
                 this,
                 mDrawer,
@@ -185,7 +157,7 @@ public class MainActivity extends BaseActivity<ActivityMainBinding, MainViewMode
         String version = getString(R.string.version) + " " + BuildConfig.VERSION_NAME;
         mMainViewModel.updateAppVersion(version);
         mMainViewModel.onNavMenuCreated();
-        setupCardContainerView();
+        // setupCardContainerView();
         subscribeToLiveData();
     }
 
