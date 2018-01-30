e66fb797e4e5369fec30fe0acf967a233fbd6ade
Tran Nam <nam.tran.flash@gmail.com>
2018-01-30 11:27:02 +0700

    Show Keyboard at the first time of login and sign up

--- app/src/main/java/com/appyhome/appyproduct/mvvm/utils/helper/SystemUtils.java
+++ app/src/main/java/com/appyhome/appyproduct/mvvm/utils/helper/SystemUtils.java
@@ -1,20 +1,10 @@
 package com.appyhome.appyproduct.mvvm.utils.helper;
 
 import android.content.Context;
-import android.telephony.TelephonyManager;
+import android.view.inputmethod.InputMethodManager;
 
 public final class SystemUtils {
-
     private SystemUtils() {
 
-    }
-
-    public static String getDeviceId(Context context) {
-        final String deviceId = ((TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId();
-        if (deviceId != null) {
-            return deviceId;
-        } else {
-            return android.os.Build.SERIAL;
-        }
     }
 }
