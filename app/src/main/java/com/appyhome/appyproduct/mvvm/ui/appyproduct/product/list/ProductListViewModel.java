21a5dfdf1eb0d49d5976c2031a7879940b94155a
Tran Nam <nam.tran.flash@gmail.com>
2018-03-10 16:31:37 +0700

    Updated retry fetching products.

--- app/src/main/java/com/appyhome/appyproduct/mvvm/ui/appyproduct/product/list/ProductListViewModel.java
+++ app/src/main/java/com/appyhome/appyproduct/mvvm/ui/appyproduct/product/list/ProductListViewModel.java
@@ -4,15 +4,24 @@ import com.appyhome.appyproduct.mvvm.data.DataManager;
 import com.appyhome.appyproduct.mvvm.data.local.db.realm.Product;
 import com.appyhome.appyproduct.mvvm.data.local.db.realm.ProductFavorite;
 import com.appyhome.appyproduct.mvvm.data.model.api.product.ProductListRequest;
+import com.appyhome.appyproduct.mvvm.data.model.api.product.ProductListResponse;
 import com.appyhome.appyproduct.mvvm.ui.base.BaseViewModel;
 import com.appyhome.appyproduct.mvvm.utils.rx.SchedulerProvider;
 import com.crashlytics.android.Crashlytics;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.Single;
+import io.reactivex.disposables.Disposable;
 
 public class ProductListViewModel extends BaseViewModel<ProductListNavigator> {
 
     private int mIdSub = ProductListActivity.ID_DEFAULT_SUB;
+    private final int RETRY_MAX_COUNT = 5;
+    private final int RETRY_TIME = 5;
 
     public ProductListViewModel(DataManager dataManager,
                                 SchedulerProvider schedulerProvider) {
@@ -22,22 +31,28 @@ public class ProductListViewModel extends BaseViewModel<ProductListNavigator> {
     public void fetchProductsByIdCategory(int idSub) {
         mIdSub = idSub;
         if (isOnline()) {
-            getCompositeDisposable().add(getDataManager()
-                    .fetchProductsByIdCategory(new ProductListRequest(mIdSub, 0))
-                    .subscribeOn(getSchedulerProvider().io())
-                    .observeOn(getSchedulerProvider().ui())
-                    .subscribe(response -> {
-                        if (response.message != null && response.message.length > 0) {
-                            addProductsToDatabase(response.message);
-                        }
-                    }, throwable -> {
-                        getNavigator().showEmptyProducts();
-                        throwable.printStackTrace();
-                        Crashlytics.logException(throwable);
-                    }));
+            Disposable disposable = Observable.create((ObservableEmitter<ProductListResponse> subscriber) -> {
+                fetchProducts().subscribe(response -> {
+                    if (response.message != null && response.message.length > 0) {
+                        addProductsToDatabase(response.message);
+                    }
+                }, throwable -> {
+                    getNavigator().showEmptyProducts();
+                    throwable.printStackTrace();
+                    Crashlytics.logException(throwable);
+                });
+            }).retryWhen(throwableObservable -> throwableObservable.zipWith(Observable.range(1, RETRY_MAX_COUNT), (n, i) -> i)
+                    .flatMap(retryCount -> Observable.timer(RETRY_TIME, TimeUnit.SECONDS))).subscribe();
+            getCompositeDisposable().add(disposable);
         } else {
             getProductsBySubCategory(mIdSub, new Product[0]);
         }
+    }
+
+    private Single<ProductListResponse> fetchProducts() {
+        return getDataManager()
+                .fetchProductsByIdCategory(new ProductListRequest(mIdSub, 0)).subscribeOn(getSchedulerProvider().io())
+                .observeOn(getSchedulerProvider().ui());
     }
 
     private void addProductsToDatabase(Product[] list) {
