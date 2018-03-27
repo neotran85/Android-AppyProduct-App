57d8fdb680dcba4c08f6aabd7561e301841de089
Tran Nam <nam.tran.flash@gmail.com>
2018-03-27 12:24:36 +0700

    Updated no extra service for air con servicing.

--- app/src/main/java/com/appyhome/appyproduct/mvvm/ui/appyservice/bookingservices/step2/ServicesStep2ViewModel.java
+++ app/src/main/java/com/appyhome/appyproduct/mvvm/ui/appyservice/bookingservices/step2/ServicesStep2ViewModel.java
@@ -1,6 +1,9 @@
 package com.appyhome.appyproduct.mvvm.ui.appyservice.bookingservices.step2;
 
+import android.databinding.ObservableField;
+
 import com.appyhome.appyproduct.mvvm.data.DataManager;
+import com.appyhome.appyproduct.mvvm.data.model.db.ServiceOrderUserInput;
 import com.appyhome.appyproduct.mvvm.ui.base.BaseViewModel;
 import com.appyhome.appyproduct.mvvm.utils.rx.SchedulerProvider;
 
@@ -8,9 +11,13 @@ import java.util.ArrayList;
 
 public class ServicesStep2ViewModel extends BaseViewModel<ServicesStep2Navigator> {
 
+    public ObservableField<Boolean> isExtraServiceVisible = new ObservableField<>(false);
+
     public ServicesStep2ViewModel(DataManager dataManager,
                                   SchedulerProvider schedulerProvider) {
         super(dataManager, schedulerProvider);
+        int type = getDataManager().getServiceOrderUserInput().getType();
+        isExtraServiceVisible.set(type == ServiceOrderUserInput.SERVICE_HOME_CLEANING);
     }
 
     public void updateServiceOrderInfo(String timeSlot1, String timeSlot2, String timeSlot3, ArrayList<String> extraServices) {
